35f744b5a1742bc10b19023b1c947e22bdcaafb7
Sridharnithya <121158254+Sridharnithya@users.noreply.github.com>
2022-12-24 17:00:17 +0530

    Add files via upload

---
+++ day4program-02.py
@@ -0,0 +1,9 @@
+
+def greet( ):
+    inpstr=input("Enter your name:")
+    outstr="Hello"+" "+inpstr
+    print(outstr)
+greet()
+print(greet('Scholar'))
+temp=input("enter your name:")
+print(greet(temp))
