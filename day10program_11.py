3d56bd28ba94e7781f493c06c3a28e0c41757821
Sridharnithya <121158254+Sridharnithya@users.noreply.github.com>
2022-12-31 17:10:18 +0530

    Add files via upload

---
+++ day10program_11.py
@@ -0,0 +1,19 @@
+import sqlite3
+carData= [
+    (1,'Audi',52642),
+    (2,'Mercedes',57127),
+    (9,'Maruthi',99999),
+    (3,'skoda',9000),
+    (4,'Volvo',29000),
+    (5,'Bentley',350000),
+    (6,'Hummer',41400),
+    (7,'Volkswagen',21600)
+    ]
+con=sqlite3.connect('mtica.db')
+cur=con.cursor()
+cur.execute("DROP TABLE IF EXISTS Cars")
+cur.execute("CREATE TABLE Cars(Id INT, Name TEXT,Price INT)")
+cur.executemany("INSERT INTO Cars VALUES(?, ?, ?)", carData)
+con.commit()
+con.close()
+print("Values inserted.")
